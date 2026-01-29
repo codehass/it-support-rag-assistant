c8e47f6e87f4f23e56f08b74cffddfd80d6b5358
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2026-01-29 12:55:22 +0100

    update user model and add Query model

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, Float
+from sqlalchemy.sql import func
+from sqlalchemy.types import DateTime
 from ..db.database import Base
 
 
@@ -6,7 +8,21 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+
+
+class Query(Base):
+    __tablename__ = "queries"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, nullable=False)
+    question = Column(String, nullable=False)
+    answer = Column(String, nullable=False)
+    cluster = Column(Integer, nullable=False)
+    latency_ms = Column(Float, nullable=False)
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )

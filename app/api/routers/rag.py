611ee9d97459a6aa178596a3daf95ce717b94494
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2026-01-30 21:27:06 +0100

    load cluster model and save the cluster id

--- app/api/routers/rag.py
+++ app/api/routers/rag.py
@@ -1,15 +1,23 @@
-from fastapi import APIRouter, Depends
+import os
 import time
+from pathlib import Path
+from fastapi import APIRouter, Depends
 from app.authentication.auth import get_current_user
 from ...schemas.user_schema import UserSchema, QueryRequest, QueryResponse
 from ...db.database import get_db
 from sqlalchemy.orm import Session
 from ...RAG.query import ITSmartAssistant
 from ...models.user_model import Query
+from ml.cluster_model import ClusterModel
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+MODEL_DIR = os.path.join(BASE_DIR, "ml", "models")
 
 router = APIRouter(prefix="/api/v1/rag", tags=["RAG Routes"])
 
 rag_instance = ITSmartAssistant()
+cluster = ClusterModel(model_path=os.path.join(BASE_DIR, "ml", "model"))
 
 
 @router.post("/query", response_model=QueryResponse)
@@ -22,14 +30,14 @@ async def get_rag_answer(
     rag_answer = rag_instance.ask(query.question)
     time_end = time.time()
     latency_ms = time_end - time_start
-
     answer = rag_answer["answer"]
 
+    cluster_id = cluster.predict_cluster(query.question)
     new_query = Query(
         user_id=current_user.id,
         question=query.question,
         answer=answer,
-        cluster="default",
+        cluster=cluster_id,
         latency_ms=latency_ms,
     )
     db.add(new_query)

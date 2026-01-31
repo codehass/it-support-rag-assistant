8d60602648e4dc7f832a5de405693395bde75fcb
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2026-01-31 11:58:47 +0100

    track cluster model with mlflow

--- ml/cluster_model.py
+++ ml/cluster_model.py
@@ -1,28 +1,50 @@
 import joblib
-import mlflow
 import numpy as np
+import mlflow
+import time
 from langchain_huggingface import HuggingFaceEmbeddings
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HF_TOKEN = os.getenv("HF_TOKEN")
 
 
 class ClusterModel:
     def __init__(self, model_path: str):
+        self.model_path = model_path
+
         self.kmeans = joblib.load(f"{model_path}/it_support_clusters.joblib")
         self.pca = joblib.load(f"{model_path}/pca_transformer.joblib")
+
+        self.embedding_model_name = "BAAI/bge-small-en-v1.5"
         self.embedding_function = HuggingFaceEmbeddings(
-            model_name="BAAI/bge-small-en-v1.5"
+            model_name=self.embedding_model_name,
+            model_kwargs={"token": HF_TOKEN},
         )
 
-    @mlflow.trace(name="Clustering_Step")
     def predict_cluster(self, question: str) -> int:
         if not question.strip():
             return -1
 
-        new_vector = self.embedding_function.embed_query(question)
-        input_data = np.array(new_vector).reshape(1, -1)
-        reduced_vector = self.pca.transform(input_data)
-        cluster_id = self.kmeans.predict(reduced_vector)
+        with mlflow.start_run(run_name="cluster_prediction", nested=True):
 
-        return int(cluster_id[0])
+            mlflow.log_param("embedding_model", self.embedding_model_name)
+            mlflow.log_param("model_path", self.model_path)
+            mlflow.log_text(question, "input_question.txt")
+
+            start = time.time()
+            new_vector = self.embedding_function.embed_query(question)
+            input_data = np.array(new_vector).reshape(1, -1)
+            reduced_vector = self.pca.transform(input_data)
+            cluster_id = self.kmeans.predict(reduced_vector)
+            latency = time.time() - start
+
+            mlflow.log_metric("latency_sec", latency)
+            mlflow.log_metric("predicted_cluster", int(cluster_id[0]))
+
+            return int(cluster_id[0])
 
 
 #  Example

fa487b547c6d80f4c433331be480feabf5c9fa58
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2026-01-31 09:20:21 +0100

    add mlflow tracking url to config

--- app/config.py
+++ app/config.py
@@ -15,6 +15,7 @@ class Settings(BaseSettings):
     FRONTEND_URL: str = Field(...)
     GOOGLE_API_KEY: str = Field(...)
     HF_TOKEN: str = Field(...)
+    MLFLOW_TRACKING_URI: str = Field(...)
 
     model_config = ConfigDict(env_file=".env", env_file_encoding="utf-8")
 

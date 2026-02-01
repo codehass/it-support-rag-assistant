96934769c190e73987ee5a4c94ceb4d8e4e8c421
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2026-02-01 15:42:43 +0100

    write test config

---
+++ tests/conftest.py
@@ -0,0 +1,42 @@
+import contextlib
+
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.db.database import Base, get_db
+from app.main import app
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+@contextlib.asynccontextmanager
+async def lifespan_override(app):
+    yield
+
+
+app.router.lifespan_context = lifespan_override
+
+
+@pytest.fixture(scope="function")
+def client():
+    with TestClient(app) as test_client:
+        yield test_client
